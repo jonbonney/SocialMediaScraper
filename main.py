d64994ab077213fc14c521169487d2bf5ebf776c
Jon <jcbonney486@gmail.com>
2022-02-20 21:20:28 -0600

    fixed catastrophic async timeout error

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def read_urls():
     # Remove all NaN (Not a Number) from list which arose from missing values in data set
     urls = [i for i in raw_urls if not pandas.isna(i)]
     print('Removed', len(raw_urls) - len(urls), 'empty URL elements from list.', sep=' ')
-    # urls = ['homedepot.com', '3m.com', 'www.7-eleven.com', 'www.aflac.com']
+    # urls = ['homedepot.com', '3m.com', 'www.7-eleven.com', 'www.aflac.com', 'https://www.battlenorthgold.com']
     print(urls)
     # Clean up beginning of URL to ensure that they all use secure https schema
     for i in range(len(urls)):
@@ -64,13 +64,15 @@ def review_url(id, url, error='ErrorUnknown', traceback='No traceback'):
 async def get_page(session, id, url, timeout, headers):
     # Request HTML code from URL
     try:
-        async with session.get(url, timeout=timeout, headers=headers) as response:
-            print(colored(('Fetching HTML from number ' + str(id) + ': '), color='yellow'), url)
+        print(colored(('Staging request number ' + str(id) + ': '), color='yellow'), url)
+        async with session.get(url, headers=headers) as response:
+
+            print('status of ' + str(id) + ': ' + str(response.status))
             page = await response.text()
     # Handle errors
-    except aiohttp.ServerTimeoutError as e:
-        print(colored(('Exception in ' + str(id) + ': Timeout'), 'red'), ' | Adding URL to review_urls: ', url)
-        review_url(id, url, 'Timeout', e)
+    except asyncio.exceptions.TimeoutError as e:
+        print(colored(('Exception in ' + str(id) + ': asyncio.Timeout'), 'red'), ' | Adding URL to review_urls: ', url)
+        review_url(id, url, 'asyncio.Timeout', e)
         return
     except aiohttp.InvalidURL as e:
         print(colored(('Exception in ' + str(id) + ': InvalidURL'), 'red'), ' | Adding URL to review_urls: ', url)
@@ -159,16 +161,20 @@ async def main():
     # Get user_agent and timeout from config file
     config = read_config()
     user_agent = config['user_agent']
+    print('user_agent: ', user_agent)
     timeout = config['timeout']
     headers = {'user-agent': user_agent}
     tasks = []
 
-    conn = aiohttp.TCPConnector(limit=30)
+    # limit connection pool to 50 (100 by default) to lessen resource needs
+    # enable_cleanup_closed=True to fix any ssl leaking caused by poor server behavior
+    conn = aiohttp.TCPConnector(limit=50, enable_cleanup_closed=True)
 
-    # Total URLs and current count for progress output
-    total = len(urls)
-    count = 0
-
+    # session versus request-specific timeouts are incredibly confusing in aiohttp
+    # by default, request-specific timeouts count the time waiting for connection pool to open up
+    # this ClientTimeout object should resolve this by specifying None for total timeout
+    timeout_seconds = 10
+    timeout = aiohttp.ClientTimeout(total=None, sock_connect=timeout_seconds, sock_read=timeout_seconds)
     async with aiohttp.ClientSession(headers=headers, connector=conn) as session:
         # Iterate through list of URLs, scraping the HTML code for each and parsing out social media links
         for i in range(len(urls)):
