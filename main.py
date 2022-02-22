e2faf13de2e4ec2b977a9a99e80015177a055273
Jon <jcbonney486@gmail.com>
2022-02-21 23:05:10 -0600

    improved is_social_account and parse_social_handle

--- main.py
+++ main.py
@@ -6,23 +6,29 @@ import asyncio
 from termcolor import colored
 
 
+def read_config():
+    # Read and return config file
+    with open("config.yml", "r") as file:
+        config = yaml.safe_load(file)
+    file.close()
+    return config
+
+
 def is_social_account(url):
     # List of the major social media platforms
-    platforms = ['youtube.com/user/',
-                 'youtube.com/channel/',
-                 'facebook.com/',
-                 'instagram.com/',
-                 'linkedin.com/company/',
-                 'tiktok.com/',
-                 'pinterest.com/']
+    platforms = [r'youtube.com/(user|channel)/.',
+                 r'facebook.com/.',
+                 r'instagram.com/.',
+                 r'linkedin.com/company/.',
+                 r'tiktok.com/@.',
+                 r'pinterest.com/.']
     # Check if URL could be an account of a major social media platforms
-    for i in platforms:
-        if re.search(i, url, re.IGNORECASE):
+    for p in platforms:
+        if re.match(r'(https://)?(http://)?(www.)?' + p, url, re.IGNORECASE):
             return True
-    # Check if the URL is a Twitter URL (separately because it requires additional condition)
-    # /i/ indicates a tweet and /hashtag/ indicates a hashtag, so we can eliminate twitter URLs that include those
-    twitter = re.search('twitter.com/', url, re.IGNORECASE)
-    tweet_or_hashtag = re.search('/i/|/hashtag/', url, re.IGNORECASE)
+    # Check twitter separately because it requires additional conditionals
+    twitter = re.match('(https://)?(http://)?(www.)?twitter.com/.', url, re.IGNORECASE)
+    tweet_or_hashtag = re.search(r'twitter.com/(i/|hashtag/)', url, re.IGNORECASE)
     if twitter and not tweet_or_hashtag:
         return True
     # If the URL has not met any of the conditions, it looks like it isn't a social account, so return False
@@ -35,7 +41,7 @@ def read_urls():
     data_path = config['data_path']
     url_col = config['url_col']
     # Read Web URL column of data set as list
-    raw_urls = pandas.read_excel(data_path)[url_col].head(3000).to_list()
+    raw_urls = pandas.read_excel(data_path)[url_col].head(1000).to_list()
     # Remove all NaN (Not a Number) from list which arose from missing values in data set
     urls = [i for i in raw_urls if not pandas.isna(i)]
     print('Removed', len(raw_urls) - len(urls), 'empty URL elements from list.', sep=' ')
@@ -108,14 +114,34 @@ async def get_page(session, url_id, url, timeout, headers):
     return page
 
 
-def parse_socials(url_id, url, page):
+async def fetch_socials(session, url_id, url, timeout, headers):
+    # Run get_page() to get the HTML page for the specified URL
+    page = await get_page(session, url_id, url, timeout=timeout, headers=headers)
+    if not page:
+        return  # if there was an error preventing the HTML code from being retrieved, skip to next URL
+    print(colored(('Received HTML from number ' + str(url_id) + ': '), color='green'), url)
+    # Run parse_socials() to get all social account links found in the HTML code
+    social_links = parse_social_links(url_id, url, page)
+
+    social_handles = []
+    for link in social_links:
+        handle = parse_social_handle(link)
+        if handle:
+            social_handles.append(handle)
+    # Log socials to socials_log
+    if social_handles:
+        print(social_handles)
+        log_socials(url, social_handles)
+
+
+def parse_social_links(url_id, url, page):
     # Use a Regular Expression pattern to search for all href links
     pattern = re.compile(r'href=(\"[^\"]*|\'[\']*)')
     matches = pattern.findall(page)
 
     # For each match, check if it looks like a social media account url and add to list if it is
     # Strip leading " or ' from links before adding to list
-    socials = [m.strip('\'\"') for m in matches if is_social_account(m)]
+    socials = [m.lstrip('\'\"') for m in matches if is_social_account(m.lstrip('\'\"'))]
 
     # Insert socials list into set to remove duplicates, then convert back to list
     socials = list(set(socials))
@@ -127,35 +153,35 @@ def parse_socials(url_id, url, page):
     return socials
 
 
+def parse_social_handle(url):
+    print('parsing for handle:', url)
+
+    # List of patterns for each of the major social media platforms
+    platforms = [r'(?P<platform>youtube).com/(user|channel)/(?P<handle>[^/?]+)',
+                 r'(?P<platform>linkedin).com/company/(?P<handle>[^/?]+)',
+                 r'(?P<platform>tiktok).com/@(?P<handle>[^/?]+)',
+                 r'(?P<platform>facebook|instagram|pinterest|twitter).com/(?P<handle>[^/?]+)']
+    # Try each pattern and return handle if match found
+    for p in platforms:
+        try:
+            pattern = re.compile(p)
+            handle = pattern.search(url, re.IGNORECASE).group('handle')
+            platform = pattern.search(url, re.IGNORECASE).group('platform')
+            return platform, handle
+        except AttributeError as e:
+            # If no match was found, an AttributeError is raised.
+            # If this happens for a URL where is_social_account returned true,
+            # it indicates that is_social_account() is too permissive.
+            continue
+    raise Exception('Social account handle could not be parsed from URL: ' + url)
+
+
 def log_socials(url, socials):
     # Log socials to socials_log.csv
     with open("socials_log.csv", "a+") as file:
-        file.write('\n' + url)
         for social in socials:
-            file.write(', ' + social)
+            file.write('\n' + url + ', ' + social[0] + '=' + social[1])
     file.close()
-
-
-def read_config():
-    # Read and return config file
-    with open("config.yml", "r") as file:
-        config = yaml.safe_load(file)
-    file.close()
-    return config
-
-
-async def fetch(session, url_id, url, timeout, headers):
-    # Run get_page() to get the HTML page for the specified URL
-    page = await get_page(session, url_id, url, timeout=timeout, headers=headers)
-    if not page:
-        return  # if there was an error preventing the HTML code from being retrieved, skip to next URL
-    print(colored(('Received HTML from number ' + str(url_id) + ': '), color='green'), url)
-    # Run parse_socials() to get all social account links found in the HTML code
-    socials = parse_socials(url_id, url, page)
-
-    # Log socials to socials_log
-    if socials:
-        log_socials(url, socials)
 
 
 async def main():
@@ -185,7 +211,7 @@ async def main():
     async with aiohttp.ClientSession(headers=headers, connector=conn) as session:
         # Iterate through list of URLs, scraping the HTML code for each and parsing out social media links
         for i in range(len(urls)):
-            tasks.append(fetch(session, i, urls[i], timeout=timeout, headers=headers))
+            tasks.append(fetch_socials(session, i, urls[i], timeout=timeout, headers=headers))
 
         # Asynchronously run all tasks
         await asyncio.gather(*tasks)
